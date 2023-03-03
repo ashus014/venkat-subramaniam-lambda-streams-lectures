b22f2cfc00e16c283ec6aa11608ceb3c312ad2b5
Ashu Singh <ashu.ask.singh@gmail.com>
2023-03-04 01:01:11 +0530

    method reference

--- src/main/java/org/ashusinghin/Main.java
+++ src/main/java/org/ashusinghin/Main.java
@@ -1,5 +1,44 @@
-package org.ashusinghin;public class Main {
+package org.ashusinghin;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        // external iterators
+        // the more you control, the more your have to change, when you have to change what you control
+//        for(int i=0; i< numbers.size(); i++) {
+//            System.out.println(numbers.get(i));
+//        }
+
+        // external iterator
+//        for(int e : numbers) {
+//            System.out.println(e);
+//        }
+
+        // internal iterator
+//        numbers.forEach(new Consumer<Integer>() {
+//            @Override
+//            public void accept(Integer integer) {
+//                System.out.println(integer);
+//            }
+//        });
+
+        // simply replacing the above anonymous class
+//        numbers.forEach(x -> System.out.println(x));
+
+        // using method reference syntax
+//        numbers.forEach(System.out::println);
+
+        // while lambdas are really cute, keep them that way
+        // avoid urge of writing large lambdas
+
+        numbers.stream()
+                .map(String::valueOf)
+                .forEach(System.out::println);
     }
 }
